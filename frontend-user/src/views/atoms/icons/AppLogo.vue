3b841605848787fb852214254fd0aca642cecb46
hiroto <hiroto.soccer.0701@gmail.com>
2024-02-12 23:52:29 +0900

    ログイン済みヘッダーコンポーネント化

---
+++ frontend-user/src/views/atoms/icons/AppLogo.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <router-link to="/">
+    <img class="h-8 w-auto" src="../../../assets/logo.svg" alt="App Logo" />
+  </router-link>
+</template>
